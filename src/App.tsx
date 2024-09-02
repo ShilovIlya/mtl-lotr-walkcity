f7546e6e7236591e76e90a523de8f164366fe57c
shilovilya <shilovilya@gmail.com>
2024-09-01 21:55:33 -0400

    feat - update intro and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import lotrfolks from './assets/lotr-folks.png';
 import './App.css';
+import IntroComponent from './components/IntroComponent';
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>City quest to bring the ring to the "Mordor".</p>
+				<img
+					src={lotrfolks}
+					className="Lotr-folks-logo"
+					alt="lotrfolks"
+				/>
+				<IntroComponent />
 				<a
 					className="App-link"
 					href="https://reactjs.org"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					Start walking Montreal Middle-Earth
+					Начать путешествие по Средиземью Монреаля
 				</a>
 			</header>
 		</div>
